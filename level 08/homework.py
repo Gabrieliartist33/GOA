de2d90df379262618c0d206124309d49c3928783
Gabrieliartist33 <khaniashviligabrieli@gmail.com>
2024-07-01 23:35:37 +0400

    homework

---
+++ level 08/homework.py
@@ -0,0 +1,19 @@
+lemur = 85
+lemur_mango = 55
+print("lemur-lemur_mango")
+
+print("Hello, world")
+
+x = 5
+print(x + 5)
+
+a = 5
+b = 2
+print(a / b)
+
+x = 5
+y = 10
+print(x*y)
+
+age = "twenty-five"
+print(20-5)
