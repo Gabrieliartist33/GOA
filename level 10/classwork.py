7135d42b5c93e92d5542814b3a58ebb46f972cc2
Gabrieliartist33 <khaniashviligabrieli@gmail.com>
2024-07-18 18:40:49 +0400

    homework

---
+++ level 10/classwork.py
@@ -0,0 +1,2 @@
+lemur_food = input ("which food do you like:")
+print(lemur_food)
---
+++ level 10/homework.py
@@ -0,0 +1,20 @@
+# 1
+name = ("Gabrieli")
+surname = ("khaniashvili")
+print("name , surname")
+# 2
+surname = input ("Gabriel khaniashvili:")
+
+# 3
+surname = input ("Gabriel khaniashvili:")
+print("what's your favorite pet: my favorite pet is dog. what's your favorite colour: my favorite color is red. what's your favorite car: my favorite car is bugatti. and what's your favorite actor: my favorite actor is uil smiti.")
+
+# 4
+# input-ი არის, მაგალითად როცა რაიმეს ვწერ ან ვლაპარაკობ,
+# შემდეგ კი ის გადასცემს output-ს. output-ი კი არის მაგალითად როცა input-ი გადასცემს თავის ნებისმიერ მოქმედებას მაგალითად 
+# ლაპარაკს ან წერას და output-ი კი გამოგიტანს ეკრანზე თუ რა დაწერა ან რას ლაპარაკობს.  
+
+# 5
+# concatenation ნიშნავს შეერთებას, მაგალითად, "თოვლის" და "ბურთის" შეერთება არის "თოვლის ბურთი", 
+# ანუ პროგრამირებაში როცა დავწერთ რაიმე სიტყვას და მის წინ დავუწერთ პლიუსის ნიშანს, კიდევ რაიმე სიტყვა, რომ დავწეროთ, 
+# ის სიტყვები ერთმანეთს შეუერთდება.
