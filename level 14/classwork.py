3baa36f9a216a72c83dd9e1b73d9daa96b99cde0
Gabrieliartist33 <khaniashviligabrieli@gmail.com>
2024-08-13 19:47:31 +0400

    homework

---
+++ level 14/classwork.py
@@ -0,0 +1,30 @@
+name = "Gabi"
+print(type(name))
+# type გამოგვიტანს ეკრანზე, რომ ეს არის str.
+
+num1 = 123
+print(type(num1))
+#type გამოგვიტანს, რომ ეს არის int.
+
+num2 = 10.0
+print(type(num2))
+#type გამოგვიტანს, რომ ეს არის float.
+
+num3 = 23.0 - 7.0
+print(type(num3))
+#type გამოგვიტანს, რომ ეს არის float.
+
+num4 = 34 - 23
+print(type(num4))
+#type გამოგვიტანს, რომ ეს არის int.
+
+# შემდეგი დავალება:
+
+gabriel_chad2 =int("1")
+print(gabriel_chad2)
+
+GaGa =str("3")
+print(GaGa)
+
+lol =float("4.0")
+print(lol)
---
+++ level 14/homework.py
@@ -0,0 +1,160 @@
+age =int("10")
+print(age + 5)
+
+num1 = "132"
+print(type(num1))
+
+num2 = 453
+print(type(num2))
+
+num3 = 4.5
+print(type(num3))
+
+num4 = 4.6 - 2.3
+print(type(num4))
+
+num5 = 34 + 24
+print(type(num5))
+
+num6 = "23" + "77"
+print(type(num6))
+
+num7 = 4.5 + 7.8
+print(type(num7))
+
+num8 = 6.7 * 3.4
+print(type(num8))
+
+num9 = 6.4 / 2.4
+print(type(num9))
+
+num10 = 4 * 46
+print(type(num10))
+
+num11 = 5 * 9
+print(type(num11))
+
+num12 = "10" + "5"
+print(type(num12))
+
+num13 = 4.4 * 9.0
+print(type(num13))
+
+num14 = 45 - 34
+print(type(num14))
+
+num15 = 2.4 * 7.9
+print(type(num15))
+
+num16 = 23
+print(type(num16))
+
+num17 = 233 - 123
+print(type(num17))
+
+num18 = 32.4 * 2.0
+print(type(num18))
+
+num19 = 6.0 / 3.0
+print(type(num19))
+
+num20 = 4.5 * 2.0
+print(type(num20))
+
+
+
+num1 =int("123")
+print(num1)
+
+num2 =int(2.6)
+print(num2)
+
+num3 =int("8")
+print(num3)
+
+num4 =int("24")
+print(num4)
+
+num5 =int("32" + "32")
+print(num5)
+
+num6 =int(45.5 + 12.2)
+print(num6)
+
+num7 =int("32" + "19")
+print(num7)
+
+num8 =int(3.4 - 2.0)
+print(num8)
+
+num9 =int("45" + "23")
+print(num9)
+
+num10 =int("34" + "9")
+print(num10)
+
+
+
+
+num11 =str(34 + 23)
+print(num11)
+
+num12 =str(5.6 + 2.3)
+print(num12)
+
+num13 =str("23" + "21")
+print(num13)
+
+num14 =str(5 + 63)
+print(num14)
+
+num15 =str(46 - 21)
+print(num15)
+
+num16 =str(2.3 - 1.0)
+print(num16)
+
+num17 =str(34 * 23)
+print(num17)
+
+num18 =str(7.8 + 3.4)
+print(num18)
+
+num19 =str("21" + "23")
+print(num19)
+
+num20 =str(32-23)
+print(num20)
+
+
+
+
+num21 =float(3 + 34)
+print(num21)
+
+num22 =float("23" + "26")
+print(num22)
+
+num23 =float(54 - 34)
+print(num23)
+
+num24 =float("43" + "2")
+print(num24)
+
+num25 =float(66 / 2)
+print(num25)
+
+num26 =float("42" + "23")
+print(num26)
+
+num27 =float(45 / 7)
+print(num27)
+
+num28 =float("344" + "466")
+print(num28)
+
+num29 =float(89 * 23)
+print(num29)
+
+num30 =float("34" + "23")
+print(num30)
