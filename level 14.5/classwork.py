805ba37235261e572705a2479455f09b585d9515
Gabrieliartist33 <khaniashviligabrieli@gmail.com>
2024-08-22 16:53:46 +0400

    homework

---
+++ level 14.5/classwork.py
@@ -0,0 +1,49 @@
+# str :
+
+print("str")
+print("12")
+print("1.0")
+
+# int : 
+
+print(123)
+print(1.0)
+print(123 - 12)
+
+# float :
+
+print(3.0 - 2.0)
+print(2.0 + 2.1)
+print(3.4 + 2.4)
+
+
+print(type("G"))
+print(type("12"))
+print(type("go go"))
+
+print(type(12))
+print(type(23 - 2))
+print(type(1))
+
+print(type(1.0 + 10.0))
+print(type(2.3))
+print(type(2.3 + 1.3))
+
+wariable =input("Enter your first number:")
+the_nexst_wariable =input("Enter your second number:")
+print(int(wariable) + int(the_nexst_wariable))
+# ორივე ცვლადის ჯამი არის 22.
+
+name=input("what's your name ?:" )
+surname =input("what's your username ?:" )
+age =input("how old are you ?:" )
+hobby =input("what's your hobby ?:" )
+
+print(str(name) + str(surname) + str(age) + str(hobby))
+
+name1 =input("what's your friend name ?:" )
+surname1 =input("what's your friend username ?:" )
+age1 =input("how old your friend ?:" )
+hobby1 =input("what's your friend hobby ?:" )
+
+print(str(name1) + str(surname1) + str(age1) + str(hobby1))
