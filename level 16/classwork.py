7f036b6bf97ef63ed1bfc0e01d7900f9255b4e85
Gabrieliartist33 <khaniashviligabrieli@gmail.com>
2024-08-26 15:57:44 +0400

    homework

---
+++ level 16/classwork.py
@@ -0,0 +1,37 @@
+print(12 >= 17 )
+print(1.2 >= 1.2)
+print(5 >= 12)
+print(60 >= 60)
+
+
+print(56 <= 50)
+print(7.7 <= 2.2)
+print(45 <= 223)
+print(36 <= 36)
+
+
+print(45 > 67)
+print(24 > 14)
+print(45 > 13)
+print(78 >89)
+
+
+print(57 < 23)
+print(65 < 90)
+print(10 < 19)
+print(16 < 13)
+
+
+print(17 != 19)
+print(25 != 56)
+print(13 != 13)
+print(17 != 99)
+
+
+print(13 == 45)
+print(45 == 45)
+print(23 == 14)
+print(90 == 90)
+
+
+ommamaolala = int(input(123))
---
+++ level 16/homework.py
@@ -0,0 +1,42 @@
+# booleani არ გამოიყენება პროგრამაში, booleani ნიშნავს მართალს და მცდარს ანუ (true and false).
+
+print(45 > 23)
+print(12 > 21)
+print(81 > 34)
+print(13 > 46)
+print(23 > 68)
+
+
+print(92 < 34)
+print(86 < 12)
+print(29 < 35)
+print(34 < 45)
+print(12 < 91)
+
+
+print(43 >= 24)
+print(53 >= 13)
+print(19 >= 12)
+print(82 >= 34)
+print(10 >= 23)
+
+
+print(12 <= 34)
+print(67 <= 12)
+print(10 <= 10)
+print(46 <= 23)
+print(19 <= 23)
+
+
+print(48 != 19)
+print(23 != 23)
+print(65 != 36)
+print(12 != 35)
+print(13 != 13)
+
+
+print(23 == 23)
+print(34 == 87)
+print(12 == 12)
+print(24 == 15)
+print(78 == 78)
