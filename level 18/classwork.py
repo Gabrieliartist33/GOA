01a850874e08f7f0122a36e9e811d0b57b3231f8
Gabrieliartist33 <khaniashviligabrieli@gmail.com>
2024-09-14 17:25:04 +0400

    commit

---
+++ level 18/classwork.py
@@ -0,0 +1,13 @@
+# print((True or (True and True)) and (False or False))
+# print((True and False) or (False and True))
+# print((False and True) or (False and False))
+# print((True or False) and (False or true))
+# print((False and True) or (True and False))
+# print((True or False) and (True and True))
+
+# 1: (true or true)->false
+# 2: (false or false)->false
+# 3: (false or false)->false
+# 4: (true and true)->true
+# 5: (false or false)->false
+# 6: (true and true)->true 
---
+++ level 18/homework.py
@@ -0,0 +1,61 @@
+# and თეგი გამოიყენება boolean-ში, boolean-ი არის true და false. მაგალითი: print(true and false), and თეგში რომც 200 True იყოს და ერთი false, მაინც false-ს გამოგიტანს.
+# or თეგი გამოიყენება boolean-ში ანუ true და false-ში. or თეგში რომც ეწეროს 200 false და ერთი true, მაინც true-ს გამოგიტანს.
+
+
+# print((True or False) and (True and True))
+# print((True or False) and (False or False))
+# print((False and True) or (True and False))
+# print((False or False) and (True or True))
+# print((True and True) or (False and True))
+# print((False or True) and (True and False))
+# print((False and False) or (False or True))
+# print((True or False) and (False or True))
+# print((True and False) or (True and False))
+# print((True and True) or (True and False))
+# print((False and False) or (True or bool(5)))
+# print((True or True) and (False and False))
+# print((False or True) and (True or True))
+# print((True and False) or (True and False))
+
+# გამარტივებული მაგალითები :
+
+# print(True and True)->True
+# print(True and False)->False
+# print(False or False)->False
+# print(False and True)->False
+# print(True or False)->True
+# print(True and False)->False
+# print(False or True)->True
+# print(True and True)->True
+# print(True or False)->True
+# print(False or True)->True
+# print(True and False)->False
+# print(True and False)->False
+# print(True and True)->True
+# print(False or False)->false
+
+# and თეგზე 10 მაგალითი :
+
+print(True and False)
+print(False and False)
+print(True and True)
+print(False and False)
+print(True and False)
+print(False and True)
+print(False and False)
+print(True and True)
+print(False and False)
+print(True and True)
+
+# or თეგზე 10 მაგალითი :
+
+print(True or False)
+print(True or True)
+print(False or False)
+print(True or True)
+print(False or True)
+print(False or False)
+print(True or True)
+print(False or True)
+print(True or True)
+print(False or False)
