c1e62119d437e1833a782b3de8c4e577b3c311ae
Gabrieliartist33 <khaniashviligabrieli@gmail.com>
2024-11-13 20:39:17 +0400

    commit

---
+++ level 24/homework.py
@@ -0,0 +1,27 @@
+#
+
+user_password = int(input("Enter your numbers :"))
+while user_password != 8:
+    user_password = int(input("Enter your numbers :"))
+
+num_1 = 0
+while num_1 < 10:
+    print(num_1)
+    num_1 = num_1 + 1
+
+num_2 = 10
+while num_2 < 100:
+    print(num_2)
+    num_2 = num_2 + 5
+
+num_3 = 20
+while num_3 > 10:
+    print(num_3)
+    num_3 = num_3 - 1
+
+num_4 = 1000
+while num_4 < 100:
+    print(num_4)
+    num_4 = num_4 - 50
+
+#
